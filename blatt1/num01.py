4835bce18d361a8e9e2cd9b010def4f583669829
wittnils <nils.witt1901@gmail.com>
2020-05-03 20:45:45 +0200

    final

--- blatt1/num01.py
+++ blatt1/num01.py
@@ -2,13 +2,11 @@
 import random
 import copy
 
-
 def calc(liste,exp,len):
     i = 2
     sum = 1./2
     while i<len+2:
         sum += liste[i-2]*2**(-i)
-        print(liste[i-2]*2**(-i))
         i = i+1
     return sum*exp
 
@@ -16,26 +14,25 @@ err = 2**32
 def checall(i,liste,exp,toApprox):
     global err, smallest
     if i!=-1:
-        checall(i-1,liste,exp,toApprox)
-        liste[i] = 1
-        checall(i-1,liste,exp,toApprox)
+        listA = copy.deepcopy(liste)
+        listB = copy.deepcopy(liste)
+        checall(i-1,listA,exp,toApprox)
+        listB[i] = 1
+        checall(i-1,listB,exp,toApprox)
     else:
-        print(liste)
         if( abs(calc(liste,exp,len)-toApprox) < err ):
-            print(liste)
             err = abs(calc(liste,exp,len) - toApprox)
             smallest = copy.deepcopy(liste)
     
 # main
-len = 2 
+len = 9 
 liste = []
-exp = 2**(-1)
+exp = 2**(-2)
 for i in range(len):
     liste.append(0)
-testl = [1,0]
 toApprox = 0.3
-checall(1,liste,exp,toApprox)
+checall(len-1,liste,exp,toApprox)
+
 print(err)
 print(smallest)
-print(calc(smallest,exp,len))
-print(calc(testl,exp,len))
+
