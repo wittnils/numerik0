2e602638ccddf94eccb1fc2e90f66f84a4c5086c
wittnils <nils.witt1901@gmail.com>
2020-05-04 20:38:12 +0200

    program works now

--- blatt1/a4.cc
+++ blatt1/a4.cc
@@ -4,8 +4,24 @@
 #include <limits>
 
 int main(){
-    double x; 
-    std::cout << "Eingabe: "; 
-    std::cin >> x; 
-    std::cout << std::setprecision(std::numeric_limits<double>::digits10 + 1) << std::sqrt(3) << std::endl;
+    double sum_x = 2;
+    double x = 1.; 
+    float sum_y = 2;
+    float y = 1.; 
+    int i = 0; 
+    while(sum_y!=1 or sum_x!=1){
+        if(sum_y != 1){
+            y = pow(10, -i);
+            sum_y = 1+y;
+        }
+        if(sum_x != 0){
+            x = pow(10,-i);
+            sum_x = 1+x;
+        }
+        std::cout << "double x: " << x << "\nfloat y: " <<  y << std::endl;
+        std::cout << "double 1+x: " << std::setprecision(std::numeric_limits<double>::digits10) << sum_x << std::endl;
+        std::cout << "float 1+x: "<< std::setprecision(std::numeric_limits<float>::digits10) << sum_y << std::endl;  
+        ++i; 
+    } 
+    std::cout << "Kleinste Zahl, die bei Addition zu keiner Nulladition führt: \n" << "double: " << x*10 << "\nfloat: " << y*10 << std::endl; 
 }
