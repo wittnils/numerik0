fea554b7d3d39c75a465f4263ff3206e4a6c34e3
Mona <bv237@stud.uni-heidelberg.de>
2020-06-02 20:43:22 +0200

    Ordner blatt01,02 erstellt

---
+++ blatt01/a4.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <limits>
+
+int main(){
+    double sum_x = 2;
+    double x = 1.; 
+    float sum_y = 2;
+    float y = 1.; 
+    int i = 0; 
+    while(sum_y!=1 or sum_x!=1){
+        if(sum_y != 1){
+            y = pow(2, -i);
+            sum_y = 1+y;
+        }
+        if(sum_x != 0){
+            x = pow(2,-i);
+            sum_x = 1+x;
+        }
+        std::cout << "double x: " << x << "\nfloat y: " <<  y << std::endl;
+        std::cout << "double 1+x: " << std::setprecision(std::numeric_limits<double>::digits10+1) << sum_x << std::endl;
+        std::cout << "float 1+x: "<< std::setprecision(std::numeric_limits<float>::digits10+1) << sum_y << std::endl;  
+        ++i; 
+    } 
+    std::cout << "Kleinste Zahl, die bei Addition zu keiner Nulladition führt: \n" << "double: " << x << "\nfloat: " << y << std::endl; 
+    std::cout << std::setprecision(std::numeric_limits<double>::digits10+26) << 1+1e-16;
+}
