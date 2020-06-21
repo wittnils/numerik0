e9e95bef9289fdc017718cae5e502e7bf95399bc
wittnils <nils.witt1901@gmail.com>
2020-06-21 15:07:17 +0200

    implemented

--- blatt07/prog_iterative_solvers.cc
+++ blatt07/prog_iterative_solvers.cc
@@ -1,7 +1,8 @@
 #include <iostream>    // notwendig zur Ausgabe
 #include <vector>
 #include "hdnum.hh"    // hdnum header
-
+#include <tgmath.h> 
+//g++ -I../hdnum/ -o prog-iterative prog_iterative_solvers.cc
 namespace hdnum {
 
   template<typename REAL>
@@ -37,7 +38,109 @@ namespace hdnum {
   private:
     std::vector<MatrixEntry> entries;
   };
+  template<typename V>
+  Vector<V> richardsonIteration(DenseMatrix<V>& A, Vector<V>& x_0, Vector<V>& b, V epsilon, V maxEig, int NumberIterations){
+    int n = A.colsize();
+    // Berechne den Defekt
+    Vector<V> d_0(n, 0.0);
+    Vector<V> d_k(n, 0.0);
+    Vector<V> x_k(n,0.0);
+    Vector<V> Ax_k(n,0.0);
+    Vector<V> v(n,0.0); 
+    x_k = x_0;
+    // Bestimme initialen Defekt
+    A.mv(Ax_k, x_0); // Ax_k = A*x_0 = A*x_k
+    d_0 = b-Ax_k; // d_0 = b - Ax_k
+    d_k = d_0; 
 
+    int i = 0;
+    while(norm(d_k) > epsilon*norm(d_0) && i<NumberIterations){
+      // 3. Löse W v = d, hier ist W^{-1} = 1/\omega I für ein geeignetes omega. hier omega = maxEig
+      v = d_k; 
+      v *= 1./maxEig; 
+      // 4. x = x + v
+      x_k = x_k + v; 
+      // d = d-Av
+      A.mv(Ax_k,v);
+      d_k = d_k - Ax_k;
+      ++i;
+    }
+    return x_k; 
+  }
+  template<typename V>
+  Vector<V> jacobi(DenseMatrix<V>& A, Vector<V>& x_0, Vector<V>& b, V epsilon, int NumberIterations){
+    int n = A.colsize(); 
+    // Berechne den Defekt
+    Vector<V> d_0(n, 0.0); // initialer Defekt
+    Vector<V> d_k(n, 0.0); // Durchlaufender Defekt
+    Vector<V> x_k(n,0.0);  // k-te Iterierte
+    Vector<V> Ax_k(n,0.0); // Zwischenspeichervariablen
+    Vector<V> v(n,0.0); 
+    // Für k=0 ist x_k = x_0
+    x_k = x_0;
+    // Bestimme initialen Defekt
+    A.mv(Ax_k, x_0); // Ax_k = A*x_0 = A*x_k
+    d_0 = b-Ax_k; // d_0 = b - Ax_k
+    d_k = d_0; // k=0 => d_k = d_0
+    int i = 0; 
+    while(norm(d_k) > epsilon*norm(d_0) && i<NumberIterations){
+      /*3. Löse W v = d. Hier ist W = diag(a_11,...,a_nn), also W^{-1} = diag(1./a_11,...,1./a_nn), damit ist
+      */
+      v = d_k;  
+      for(int k = 0; k < n; ++k){
+        v[k] *= 1./A[k][k];
+      }
+      // 4. x = x + v
+      x_k = x_k + v; 
+      // d = d-Av
+      A.mv(Ax_k,v);
+      d_k = d_k - Ax_k;
+      ++i;
+    }
+    return x_k;
+  }
+  template<typename V>
+  Vector<V> gauss_seidel(DenseMatrix<V>& A, Vector<V>& x_0, Vector<V>& b, V epsilon, int NumberIterations){
+     int n = A.colsize(); 
+    // Berechne den Defekt
+    Vector<V> d_0(n, 0.0); // initialer Defekt
+    Vector<V> d_k(n, 0.0); // Durchlaufender Defekt
+    Vector<V> x_k(n,0.0);  // k-te Iterierte
+    Vector<V> Ax_k(n,0.0); // Zwischenspeichervariablen
+    Vector<V> v(n,0.0); 
+    DenseMatrix<V> W_i(n,n,0.0);
+    // Für k=0 ist x_k = x_0
+    x_k = x_0;
+    // Bestimme initialen Defekt
+    A.mv(Ax_k, x_0); // Ax_k = A*x_0 = A*x_k
+    d_0 = b-Ax_k; // d_0 = b - Ax_k
+    d_k = d_0; // k=0 => d_k = d_0
+    int i = 0;
+    // Stelle W^{-1} auf
+    for(int i = 0; i<n; ++i){
+      for(int j = 0; j <= i; ++j){ //Elemente über der Hauptidag. sind 0
+        if(j==i){
+          W_i[i][j] = 1./A[i][j];
+        }else{
+          for(int k = j; k < i-1; ++k){
+            W_i[i][j] -= A[i][k]*W_i[j][k];
+          }
+          W_i[i][j] *= 1./A[i][i];
+        }
+      }
+    }
+    while(norm(d_k) > epsilon*norm(d_0) && i<NumberIterations){
+      // 3. Löse W v = d. Hier ist W = L+D
+      W_i.mv(v,d_k);
+      // 4. x = x + v
+      x_k = x_k + v; 
+      // d = d-Av
+      A.mv(Ax_k,v);
+      d_k = d_k - Ax_k;
+      ++i;
+    }
+    return x_k;
+  }
 }
 
 int main ()
@@ -61,9 +164,31 @@ int main ()
   hdnum::Vector<double> x(N, 0.0);
   hdnum::Vector<double> b(N, 1.0);
 
+
+
+  
   // Lösen Sie nun A*x=b iterativ
 
+  //Wir können anhand der gegebenen Matrix die Überschätzung aus Satz 14.2 aus den Gerschgorin Kreisen die Eigenwerte abschätzen 
+  // \lvert -2 - \xi \rvert \le 2 \implies A hat die größten EW 4 
+  
+  // Richardson-Verfahren. B = 1/\maxEig \cdot I, aus Satz 14.1 erhalten wir, dass für alle \maxEig mit \maxEig \le \lambda_{max}^{-1}(BA)
+  // Also wegen B=I hier der Alogirthmus konvergiert für alle \maxEig mit \maxEig \le \lambda_{max}^{-1}(A). Aus den Gerschgorin-Kreisen 
+  // erhalten wir die Abschätzung (s. Matrixstruktur), dass \vert \lambda \vert \le 4. Offensichtlich ist A negativ definit und dann ist
+  // Ist setzen wir maxEig = -4
+ double maxEig = -4.; 
+ int NumberIterations = 200;
 
-
+ hdnum:: Vector<double> resultGS(N, 0.0);
+ hdnum:: Vector<double> resultJac(N, 0.0);
+ hdnum:: Vector<double> resultRichardson(N, 0.0);
+ 
+ resultRichardson = hdnum::richardsonIteration(A, x,  b, pow(10,-4), maxEig, NumberIterations);
+ resultJac = hdnum::jacobi(A, x,  b, pow(10,-4),NumberIterations);
+ resultGS = hdnum::gauss_seidel(A, x,  b, pow(10,-4),NumberIterations);
+ std::cout << resultGS << std::endl;
+ std::cout << resultJac << std::endl;
+ std::cout << resultRichardson << std::endl;
+ 
 
 }
