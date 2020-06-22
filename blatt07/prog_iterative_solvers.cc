7163b24c4806947c7fc888dc37849266cd65efb8
wittnils <nils.witt1901@gmail.com>
2020-06-22 14:32:57 +0200

    änderungen

--- blatt07/prog_iterative_solvers.cc
+++ blatt07/prog_iterative_solvers.cc
@@ -103,14 +103,13 @@ namespace hdnum {
   }
   template<typename V>
   Vector<V> gauss_seidel(DenseMatrix<V>& A, Vector<V>& x_0, Vector<V>& b, V epsilon, int NumberIterations){
-     int n = A.colsize(); 
     // Berechne den Defekt
     Vector<V> d_0(n, 0.0); // initialer Defekt
     Vector<V> d_k(n, 0.0); // Durchlaufender Defekt
     Vector<V> x_k(n,0.0);  // k-te Iterierte 
     Vector<V> Ax_k(n,0.0); // Zwischenspeichervariablen
     Vector<V> v(n,0.0); 
-    
+    int counter = 0; 
     // Für k=0 ist x_k = x_0
     x_k = x_0;
     // Bestimme initialen Defekt
@@ -120,35 +119,14 @@ namespace hdnum {
     int i = 0;
 
     // Stelle W^{-1} auf, W^{-1} ist ebenfalls untere Dreiecksmatrix
-    /*for(int i = 0; i<n; ++i){
-      for(int j = 0; j <= i; ++j){ //Elemente über der Hauptidag. sind 0
-        if(j==i){
-          // Wegen W, W^{-1} untere Dreiecksmatrix 
-          W_i[i][j] = 1./A[i][j];
-        }else{
-          // Wir haben insg., dass (L+D)*W^{-1} = I nach Konstr. 
-          // Wir bauen W_i schritweise auf und wir konstruieren hier das Element W_i[i][j]. Alle Zeilen darüber
-          // wurden bereits konstruiert. 
-          // Wenn wir die i-te Zeile von A mit der j-ten Spalte von W_i mult. kommen folgende Fälle auf
-          // 1. Für k > i ist A[i][k] = 0   2. Für m < j ist W_i[m][j] = 0
-          // D.h. wir erhalten insg., dass nur für die Indizes j <= j < i-1 beie Faktoren != 0 sind. 
-          // Wir wollen, dass es insg. Null ergibt => bilden also das "Skalarprodukt" und teilen durch -1./A[i][i]
-          for(int k = j; k < i-1; ++k){
-            W_i[i][j] -= A[i][k]*W_i[j][k];
-          }
-          // und wir teilen durch das HD-Element
-          W_i[i][j] *= 1./A[i][i];
-        }
-      }
-    }
-    */
+    
     while(norm(d_k) > epsilon*norm(d_0) && i<NumberIterations){
       // 3. Löse W v = d. 
       // Wir setzen insg. v = W^{-1} d = W_i (b-Ax_k) = W_i b - W_i Ax_k = W_i b - (L+D)^{-1}(L+D+U) x_k = W_i b - (L+D)^{-1}(L+D)x_k +(L+D)^{-1}(U) x_k
       // = W_i b - x_k + W_i U x_k = W_i(b-Ux_k) <=> x_k + v = x_k+1 = W_i (b-Ux_k) => (L+D)x_k+1 = b - Ux_k = Lx_k+1 + Dx_k+1 = b-Ux_k 
       // => Dx_k+1 = b - Ux_k - Lx_k+1 => x_k+1 = D^{-1} *(b-Ux_k -Lx_k+1)
       v = x_k; // v hier x_k
-      for(int i=0; i<n; ++i){
+      for(int i=0; i<A.entries.size(); ++i){
         x_k[i] = b[i];
         for(int m = 0; m < i; ++m){
           x_k[i] -= A[i][m]*x_k[m];
@@ -160,9 +138,30 @@ namespace hdnum {
       }
       A.mv(Ax_k,x_k);
       d_k = b - Ax_k;
-      ++i;
+      ++i; ++counter;
     }
+    std::cout << counter; 
     return x_k;
+  }
+  template<typename V> 
+  Vector<V> gauss_seidel_SPARSE(SparseMatrix<V>& A, Vector<V>& x_0, Vector<V>& b, V epsilon, int NumberIterations){
+    int n = A.colsize(); 
+    // Berechne den Defekt
+    Vector<V> d_0(n, 0.0); // initialer Defekt
+    Vector<V> d_k(n, 0.0); // Durchlaufender Defekt
+    Vector<V> x_k(n,0.0);  // k-te Iterierte 
+    Vector<V> Ax_k(n,0.0); // Zwischenspeichervariablen
+    Vector<V> v(n,0.0); 
+    int counter = 0; 
+    // Für k=0 ist x_k = x_0
+    x_k = x_0;
+    // Bestimme initialen Defekt
+    A.mv(Ax_k, x_0); // Ax_k = A*x_0 = A*x_k
+    d_0 = b-Ax_k; // d_0 = b - Ax_k
+    d_k = d_0; // k=0 => d_k = d_0
+    int i = 0;
+    V pivot = 0.0;
+    
   }
 }
 
@@ -189,7 +188,12 @@ int main ()
   hdnum::Vector<double> x(N, 0.0);
   hdnum::Vector<double> LinSolve_x(N, 0.0);
   hdnum::Vector<double> b(N, 1.0);
-
+  hdnum::SparseMatrix<double> A_S;
+  for(int i = 0; i < N; ++i){
+    for(int k = 0; k< N; ++k){
+      A_S.AddEntry(i,k,A[i][k]);
+    }
+  }
 
 
   
@@ -203,7 +207,7 @@ int main ()
   // erhalten wir die Abschätzung (s. Matrixstruktur), dass \vert \lambda \vert \le 4. Offensichtlich ist A negativ definit und dann ist
   // Ist setzen wir maxEig = -4
  double maxEig = -3.; 
- int NumberIterations = 1000;
+ int NumberIterations = 100000;
 
  
  hdnum:: Vector<double> resultGS(N, 0.0);
@@ -212,19 +216,19 @@ int main ()
  outfile << N << " "; 
  
  auto begin = std::chrono::high_resolution_clock::now();
- resultRichardson = hdnum::richardsonIteration(A, x,  b, pow(10,-4), maxEig, NumberIterations);
+ //resultRichardson = hdnum::richardsonIteration(A, x,  b, pow(10,-4), maxEig, NumberIterations);
  auto diff = std::chrono::high_resolution_clock::now() - begin;
  auto t = std::chrono::duration_cast<std::chrono::microseconds>(diff); 
  outfile << t.count() << " ";
 
  begin = std::chrono::high_resolution_clock::now();
- resultJac = hdnum::jacobi(A, x,  b, pow(10,-4),NumberIterations);
+ //resultJac = hdnum::jacobi(A, x,  b, pow(10,-4),NumberIterations);
  diff = std::chrono::high_resolution_clock::now() - begin;
  t = std::chrono::duration_cast<std::chrono::microseconds>(diff); 
  outfile << t.count() << " ";
 
  begin = std::chrono::high_resolution_clock::now();
- resultGS = hdnum::gauss_seidel(A, x,  b, pow(10,-4),NumberIterations);
+ resultGS = hdnum::gauss_seidel_SPARSE(A_S, x,  b, pow(10,-4),NumberIterations);
  diff = std::chrono::high_resolution_clock::now() - begin;
  t = std::chrono::duration_cast<std::chrono::microseconds>(diff); 
  outfile << t.count() << " ";
@@ -236,10 +240,29 @@ int main ()
  outfile << t.count() << " " << std::endl;
 
 
- std::cout << resultGS << std::endl;
- std::cout << resultJac << std::endl;
- std::cout << resultRichardson << std::endl;
- 
- std::cout << LinSolve_x << std::endl;
+
 
 }
+// So kann man das Inverse direkt berechnen, ist aber langsam 
+/*for(int i = 0; i<n; ++i){
+      for(int j = 0; j <= i; ++j){ //Elemente über der Hauptidag. sind 0
+        if(j==i){
+          // Wegen W, W^{-1} untere Dreiecksmatrix 
+          W_i[i][j] = 1./A[i][j];
+        }else{
+          // Wir haben insg., dass (L+D)*W^{-1} = I nach Konstr. 
+          // Wir bauen W_i schritweise auf und wir konstruieren hier das Element W_i[i][j]. Alle Zeilen darüber
+          // wurden bereits konstruiert. 
+          // Wenn wir die i-te Zeile von A mit der j-ten Spalte von W_i mult. kommen folgende Fälle auf
+          // 1. Für k > i ist A[i][k] = 0   2. Für m < j ist W_i[m][j] = 0
+          // D.h. wir erhalten insg., dass nur für die Indizes j <= j < i-1 beie Faktoren != 0 sind. 
+          // Wir wollen, dass es insg. Null ergibt => bilden also das "Skalarprodukt" und teilen durch -1./A[i][i]
+          for(int k = j; k < i-1; ++k){
+            W_i[i][j] -= A[i][k]*W_i[j][k];
+          }
+          // und wir teilen durch das HD-Element
+          W_i[i][j] *= 1./A[i][i];
+        }
+      }
+    }
+    */
